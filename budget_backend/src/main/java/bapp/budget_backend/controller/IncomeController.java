3d3f79cb541fd1b46f55893bfc2eced97f947b36
ethankook <93116378+ethankook@users.noreply.github.com>
2025-04-05 14:47:20 -0700

    Income controller/ income repository updates

--- budget_backend/src/main/java/bapp/budget_backend/controller/IncomeController.java
+++ budget_backend/src/main/java/bapp/budget_backend/controller/IncomeController.java
@@ -6,6 +6,28 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
+
+@RestController
+@RequestMapping("/income")
 public class IncomeController{
     
+    @Autowired
+    private IncomeService IncomeService;
+
+    @PostMapping("/add")
+    public ResponseEntity<String> addIncome(@RequestBody Income income){
+        try{
+            IncomeService.addIncome(income);
+            return ResponseEntity.ok("Income added successfully!");
+        } catch (Exception e){
+            return ResponseEntity.badRequest().body("Failed to add income: " + e.getMessage());
+        }
+    }
+
+    @GetMapping("/view")
+    public ResponseEntity<List<Income>> viewIncomes(@PathVariable Long userId){
+        List<Income> incomes = IncomeService.getIncomesById(userId);
+        return ResponseEntity.ok(incomes);
+    }
 }
--- budget_backend/src/main/java/bapp/budget_backend/repository/IncomeRepository.java
+++ budget_backend/src/main/java/bapp/budget_backend/repository/IncomeRepository.java
@@ -3,6 +3,9 @@ package bapp.budget_backend.repository;
 import bapp.budget_backend.models.Income;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface IncomeRepository extends JpaRepository<Income, Long> {
     // findAll(), save(), findById(), deleteById(), count()
+    List<Income> findByUserId(Long userId);
 }

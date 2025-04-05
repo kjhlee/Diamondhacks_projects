6e4b700ec74a9bf61710e93f41f6168da9626356
KJ Lee <kunnyjlee@gmail.com>
2025-04-05 11:49:19 -0700

    added user model

---
+++ budget_backend/src/main/java/bapp/budget_backend/models/User.java
@@ -0,0 +1,27 @@
+package bapp.budget_backend.models;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+
+
+@Entity
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Email
+    @NotBlank
+    @Column(unique = true, nullable = false)
+    private String email;
+
+    @NotBlank
+    private String password;
+}

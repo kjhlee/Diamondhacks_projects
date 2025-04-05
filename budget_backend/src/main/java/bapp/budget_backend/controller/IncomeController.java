971674c3e15cb52395cc1a110b2ac6878ef6a8fe
ethankook <93116378+ethankook@users.noreply.github.com>
2025-04-05 14:12:34 -0700

    income controller

--- budget_backend/src/main/java/bapp/budget_backend/controller/IncomeController.java
+++ budget_backend/src/main/java/bapp/budget_backend/controller/IncomeController.java
@@ -5,3 +5,17 @@ import bapp.budget_backend.services.IncomeService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/income")
+public class IncomeController{
+    @Autowired
+    private IncomeService incomeService;
+
+    @PostMapping("/add")
+    public ResponseEntity<String> addIncome(@RequestBody Income income){
+        incomeService.addIncome(income);
+        return ResponseEntity.ok("Income added successfully!");
+    }
+
+}

4b6e8add6f9225302f246b25f11121bdb1bc718b
KJ Lee <kunnyjlee@gmail.com>
2025-04-05 12:04:07 -0700

    changed so application.properties is ignored

---
+++ budget_backend/src/main/java/bapp/budget_backend/repository/UserRepo.java
@@ -0,0 +1,11 @@
+package bapp.budget_backend.repository;
+
+import java.util.Optional;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import bapp.budget_backend.models.User;
+
+public interface UserRepo extends JpaRepository<User, Long>{
+    Optional<User> findByEmail(String email);
+}
